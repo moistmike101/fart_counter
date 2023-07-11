b87bbf58570aa91fa84e606f2b74ae1ae89f854c
Mike <103404760+moistmike101@users.noreply.github.com>
2023-07-10 22:20:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function subtract() {
 }
 
 function save() {
-    let countStr = count + " "
+    let countStr = count + " | "
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
